149b2aeef0190def637ba35be2d2e530ed2a2311
Kauan Rafael Marques da Fonseca <kauan.fonseca@lead.fortaleza>
2025-08-12 16:53:08 -0300

    Entendimento básico das diretivas principais.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
-import { ItemComponent } from './item/item.component';
+import { ItemComponent } from './list/item/item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListComponent,
-    ItemComponent
+    ItemComponent,
   ],
   imports: [
     BrowserModule
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ItemComponent } from '../item/item.component';
+import { ItemComponent } from './item/item.component';
 
 @Component({
   selector: 'app-list',
@@ -7,11 +7,19 @@ import { ItemComponent } from '../item/item.component';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
+  items_unparsed = sessionStorage.getItem("items")
+  items = []
 
-  items: ItemComponent[] = []
-  
   constructor() { }
 
   ngOnInit(): void {
+    if (this.items_unparsed === null) {
+      let a = [{title: "coisa", done: false}]
+      this.items_unparsed = JSON.stringify(a)
+      sessionStorage.setItem("items", this.items_unparsed)
+    }
+
+    this.items = JSON.parse(this.items_unparsed)
   }
+
 }
